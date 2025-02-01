9649b4e5f3b03e8b26417dc2d8c53db74e5d4928
NamNguyenSparta <155911583+NamNguyenSparta@users.noreply.github.com>
2025-02-01 18:23:37 -0500

    Create data.ts
    testing

---
+++ src/app/data.ts
@@ -0,0 +1,10 @@
+//Testing image and sounds mainly
+export const images = [
+    { src: '/images/common1.jpeg', rarity: 'garbage', sound: '/sounds/get out (basic).mp3' },
+    { src: '/images/common2.jpeg', rarity: 'garbage', sound: '/sounds/get out (basic).mp3' },
+    { src: '/images/rare1.png', rarity: 'mid', sound: '/sounds/bruh (common).mp3' },
+    { src: '/images/rare2.jpeg', rarity: 'mid', sound: '/sounds/bruh (common).mp3' },
+    { src: '/images/epic1.jpeg', rarity: 'okay', sound: '/sounds/rizz (rare).mp3' }, // Fixed path
+    { src: '/images/epic2.jpeg', rarity: 'okay', sound: '/sounds/rizz (rare).mp3' }, // Fixed path
+    { src: '/images/legendary.jpeg', rarity: 'legendary', sound: '/sounds/ta da (legendary).mp3' },
+  ];
